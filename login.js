bd8cc205b7bf9d610b49118943a2038d38680a40
zin7364 <1298751101@qq.com>
2026-04-16 15:08:26 +0800

    Remove login form event listener code

--- login.js
+++ login.js
@@ -1,16 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var loginform = document.getElementById("loginform");
-    var usernameinput = document.getElementById("username");
-    var passwordinput = document.getElementById("password");
-    loginform.onsubmit = function (e) {
-        e.preventDefault();   
-        var username = usernameinput.value;
-        var password = passwordinput.value;
-        if (username && password) {
-            alert("Login Successfully");
-        } else {
-            alert("Please enter username and password");
-            passwordinput.value = "";
-        }
-    };
-});
+Not this file.I don not know how to delete it.
