9077e918f1ac6b53d250fe661f85fdd0dd545452
zin7364 <1298751101@qq.com>
2026-04-16 15:08:49 +0800

    Refactor login form validation and alerts

--- js/login.js
+++ js/login.js
@@ -3,17 +3,14 @@ document.addEventListener("DOMContentLoaded", function () {
     var usernameinput = document.getElementById("username");
     var passwordinput = document.getElementById("password");
     loginform.onsubmit = function (e) {
-        e.preventDefault();
+        e.preventDefault();   
         var username = usernameinput.value;
         var password = passwordinput.value;
-        if (username == "" || password == "") {
-            alert("Please fill in the username and password");
-            return;
-        }if (username == "zin" && password == "1298751101") {
-            alert("Successfully Login");
-            window.location.href = "add-car-page.html";
+        if (username && password) {
+            alert("Login Successfully");
         } else {
-            alert("Your username or password is incorrect.");
+            alert("Please enter username and password");
             passwordinput.value = "";
-        }};
+        }
+    };
 });
