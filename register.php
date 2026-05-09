20182823af088d203757e532518b4db028b93a1f
Doudouble123 <3024579532@qq.com>
2026-05-09 18:32:58 +0800

    Update register.php

--- register.php
+++ register.php
@@ -1,34 +1,21 @@
 <?php
-include 'conn.php';
+include "conn.php";
 
-$username = $_POST['username'];
-$password = $_POST['password'];
-$fullname = $_POST['full_name'];
-$address = $_POST['address'];
-$phone = $_POST['phone'];
-$email = $_POST['email'];
-$id_card = $_POST['id_card'];
-$seller_type = $_POST['seller_type'];
+$username   = $_POST['username'];
+$password   = password_hash($_POST['password'], PASSWORD_DEFAULT);
+$full_name  = $_POST['full_name'];
+$phone      = $_POST['phone'];
+$email      = $_POST['email'];
+$address    = $_POST['address'];
+$id_number  = $_POST['id_number'];
+$seller_type= $_POST['seller_type'];
 
-$id_photo = '';
-if (!empty($_FILES['id_photo']['name'])) {
-    $target_dir = "uploads/";
-    if (!is_dir($target_dir)) {
-        mkdir($target_dir, 0777, true);
-    }
-    $target_file = $target_dir . basename($_FILES["id_photo"]["name"]);
-    move_uploaded_file($_FILES["id_photo"]["tmp_name"], $target_file);
-    $id_photo = $target_file;
+$sql = "INSERT INTO sellers(username,password,full_name,phone,email,address,id_number,seller_type)
+VALUES('$username','$password','$full_name','$phone','$email','$address','$id_number','$seller_type')";
+
+if(mysqli_query($link,$sql)){
+    echo "<script>alert('Registration Success!');location.href='login.html';</script>";
+}else{
+    echo "<script>alert('Error!');history.back();</script>";
 }
-
-$sql = "INSERT INTO sellers (username, password, fullname, phone, email, address, id_card, id_photo, seller_type)
-VALUES ('$username', '$password', '$fullname', '$phone', '$email', '$address', '$id_card', '$id_photo', '$seller_type')";
-
-if (mysqli_query($conn, $sql)) {
-    echo "<script>alert('Registration Success!'); window.location.href='login.html';</script>";
-} else {
-    echo "<script>alert('Error: " . mysqli_error($conn) . "'); history.back();</script>";
-}
-
-mysqli_close($conn);
 ?>
